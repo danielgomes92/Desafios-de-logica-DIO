bf97c019dd4c679e808429e67973b16f9d247c2a
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-01-17 18:24:47 -0300

    solução finalizada com sucesso.

---
+++ Desafios Intermediarios/Elemento Majoritário/Solucao.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            num[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(MajorityElement(num));
+
+    }
+    public static int MajorityElement(int[] nums)
+    {
+        int major = nums[0];
+        int count = 1;
+        for (int i = 0; i < major; i++)
+        {
+            if (count < major)
+            {
+                major = nums[i];
+                count++;
+            }
+            else
+            {
+                if (major == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        }
+        return major;
+    }
+}
