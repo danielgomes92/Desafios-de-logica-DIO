f636f6b2484d61a86d9e5f67ef5344483b18ed79
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-01-17 11:27:24 -0300

    Desafio dois adicionado.

---
+++ Desafios Basicos/Encaixa ou Nao Encaixa/DesafioDois.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using System.Linq;
+
+class {
+
+    static void Main(string[] args)
+{
+    int qt = int.Parse(Console.ReadLine());
+    // TODO: Crie as outras condições necessárias para a resolução do desafio:
+    for (                 )
+    {
+
+        string a = v[0];
+        string b = v[1];
+
+        if (                  )
+            Console.WriteLine("nao encaixa");
+        else if (                                )
+            Console.WriteLine("encaixa");
+        else
+            Console.WriteLine("nao encaixa");
+
+    }
+
+}
+
+}
