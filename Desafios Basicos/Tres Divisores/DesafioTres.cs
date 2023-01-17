3ebd9719735769482d04cc96fb2251500661fa17
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-01-17 13:14:42 -0300

    Desafio três adicionado

---
+++ Desafios Basicos/Tres Divisores/DesafioTres.cs
@@ -0,0 +1,56 @@
+/************************************
+- Desafio
+Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos.
+Caso contrário, retorne false. O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m. 
+
+- Entrada
+O arquivo de entrada consiste em um inteiro n que será validada caso esteja sob as condições do desafio.
+
+- Saída
+A saída consistirá em um tipo booleano: true ou false. Como nos exemplos a baixo:
+
+---- Exemplo 1 ----
+
+- Entrada:
+2
+
+- Saída:
+false
+
+* Explicação: o valor 2 possui apenas dois divisores: 1 e 2.
+
+---- Exemplo 2 ----
+
+- Entrada:
+4
+
+- Saída:
+true
+
+* Explicação: o valor 4 tem três divisores: 1, 2 e 4.
+ ************************************/
+
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            // TODO: Crie as outras condições necessárias para a resolução do desafio:
+            if (n % i ==     )
+            {
+                count++;
+            }
+            if (count >      )
+            {
+                Console.WriteLine(false);
+            }
+        }
+        Console.WriteLine(count == 3);
+    }
+}
